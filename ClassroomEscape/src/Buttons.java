79ff2cf66c2adc3e05bfbf9f7da2100afe6d9256
Audrey030 <75454987+Audrey030@users.noreply.github.com>
2021-01-14 11:57:58 -0500

    Add files via upload

---
+++ ClassroomEscape/src/Buttons.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author audre
+ */
+//public class Buttons {
+    //public void GoToMainMenu()
+    //{
+        //this.setVisible(false);
+        //MainMenu frame2 = new MainMenu();
+        //frame2.setVisible(true);
+    //}
+//}
